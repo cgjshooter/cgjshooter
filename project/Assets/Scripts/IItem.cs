ea1e9bffb01eab8a19c1803cb03c696b443a2720
Henri Sarasvirta <henri.sarasvirta@gmail.com>
2018-07-09 13:18:15 +0300

    Added interfaces for items & modifiers (incomplete interfaces.)

---
+++ project/Assets/Scripts/IItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IItem {
+    
+    //Is the item toggleable or not.
+    bool toggleable { get; }
+    //Is the item activated immediatly on pickup
+    bool useOnPickup { get; }
+    //How many times the item can be used. -1 for infinite.
+    int useCount { get; }
+
+    void activate();
+	
+}
---
+++ project/Assets/Scripts/IModifier.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IModifier  {
+    int minimumPlayers { get; }
+    int maximumPlayers { get; }
+    string description { get; }
+
+    void apply();
+    
+
+}

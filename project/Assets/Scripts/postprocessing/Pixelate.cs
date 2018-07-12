78e70ebbf5c1b0a590c42e1b3321f0b0e3aa244f
Henri Sarasvirta <henri.sarasvirta@gmail.com>
2018-07-12 12:43:21 +0300

    Added pixelate shader.

---
+++ project/Assets/Scripts/postprocessing/Pixelate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[Serializable]
+[PostProcess(typeof(PixelateRenderer), PostProcessEvent.AfterStack, "Custom/Pixelate")]
+public sealed class Pixelate : PostProcessEffectSettings
+{
+    public Vector2Parameter pixelate = new Vector2Parameter { value = new Vector2(Screen.width, Screen.height) };
+}
+
+public sealed class PixelateRenderer : PostProcessEffectRenderer<Pixelate>
+{
+    public override void Render(PostProcessRenderContext context)
+    {
+        var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/Pixelate"));
+        sheet.properties.SetVector("_Pixelate", settings.pixelate);
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+    }
+}
---
+++ project/Assets/Scripts/postprocessing/Pixelate.shader
@@ -0,0 +1,36 @@
+﻿Shader "Hidden/Custom/Pixelate"
+{
+	HLSLINCLUDE
+
+#include "PostProcessing/Shaders/StdLib.hlsl"
+
+		TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
+	
+	float2 _Pixelate;
+
+
+	float4 Frag(VaryingsDefault i) : SV_Target
+	{
+		float2 tc = floor(i.texcoord.xy * _Pixelate) / _Pixelate;
+		
+		float4 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, tc);
+		return color;
+	}
+
+		ENDHLSL
+
+		SubShader
+	{
+		Cull Off ZWrite Off ZTest Always
+
+			Pass
+		{
+			HLSLPROGRAM
+
+#pragma vertex VertDefault
+#pragma fragment Frag
+
+			ENDHLSL
+		}
+	}
+}
